a3dd43d218a8e0514a2c393c7f8fc086156267aa
Kidus <98009847+kid-us@users.noreply.github.com>
2023-10-28 12:13:16 -0700

    Delete src/components/Dashboard directory

--- src/components/Dashboard/MyGroup.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MyGroup = () => {
-  return <div>MyGroup</div>;
-};
-
-export default MyGroup;
--- src/components/Dashboard/Supscription.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Supscription = () => {
-  return <div>Supscription</div>;
-};
-
-export default Supscription;
