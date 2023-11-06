9b4aa5fd74e11ab1367b96d2a1b5521882c3e19f
Kidus <98009847+kid-us@users.noreply.github.com>
2023-11-06 09:08:21 -0800

    Update Register.jsx

--- src/components/Pages/Register.jsx
+++ src/components/Pages/Register.jsx
@@ -1,84 +1,56 @@
-import React, { useState } from "react";
-import Joi from "joi-browser";
-import Input from "../Form/Input";
-import { Link, redirect } from "react-router-dom";
-import Button from "../Form/Button";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import Button from "./Button";
 import "../../app.css";
 
 const Register = () => {
-  const [user, setUser] = useState({
-    phone: "",
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      phoneNo: "",
+    },
   });
 
-  const [errors, setErrors] = useState({});
-
-  const schema = {
-    email: Joi.string().label("Email").email({
-      minDomainSegments: 2,
-    }),
-    phone: Joi.number().min(9).required().label("Phone"),
+  const onSubmit = (data) => {
+    console.log("251" + data.phoneNo);
   };
-
-  // Form Validation
-  const validate = () => {
-    const { error } = Joi.validate(user, schema, { abortEarly: false });
-
-    if (!error) return null;
-
-    const errors = {};
-    for (let item of error.details) errors[item.path[0]] = item.message;
-    return errors;
-  };
-
-  // Form Submit
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const errors = validate();
-    setErrors(errors || {});
-    if (errors) return;
-
-    doSubmit();
-  };
-
-  // All good to go
-  const doSubmit = () => {
-    console.log("Submit");
-  };
-
-  // On Input change
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    let userData = { ...user };
-    userData[name] = value;
-    setUser(userData);
-    setErrors(errors);
-  };
-
   return (
     <>
       <div className="container">
         <div className="row justify-content-center mt-5">
           <div className="col-lg-5 col-md-6 col-11 shadow-lg rounded py-3 form-bg">
-            <form onSubmit={handleSubmit}>
-              <p className="fw-semibold fs-4 mb-5">Sign Up</p>
-
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <p className="fw-semibold fs-4 mb-5">Sign In</p>
               <label htmlFor="phone" className="small label">
                 Enter your phone number here
               </label>
 
-              <Input
-                name="phone"
-                type="tel"
-                value={user.phone}
-                onChange={handleChange}
-                error={errors.phone}
-              ></Input>
+              <div className="input-group mb-3 fw-semibold mt-3">
+                <span className="input-group-text p-2 text- fs-6 fw-semibold">
+                  +251
+                </span>
 
-              <Button name={"Sign Up"}></Button>
+                <input
+                  type="tel"
+                  className={`form-control fs-5 ${
+                    errors.phoneNo && "border border-danger border-3"
+                  }`}
+                  {...register("phoneNo", {
+                    required: true,
+                    minLength: 9,
+                    maxLength: 9,
+                  })}
+                />
+              </div>
+
+              <Button name={"Sign In"}></Button>
 
               <p className="mt-4 small text-end fw-semibold">
-                Already have an account?
+                Don't have an account?
                 <Link
                   className="text-decoration-none ms-2 fw-semibold"
                   to={"/login"}
