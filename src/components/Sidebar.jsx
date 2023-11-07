7413724e09c6f1ebbcf9418cd7646f9a398d0e81
Kidus <98009847+kid-us@users.noreply.github.com>
2023-11-07 12:42:15 -0800

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,1 +1,18 @@
+import React from "react";
+import LargeDevice from "./Sidebar/LargeDevice";
+import MediumDevice from "./Sidebar/MediumDevice";
+import { tabs } from "./constant/tabs";
+const Sidebar = () => {
+  return (
+    <>
+      {/* Large Device Sidebar */}
+      <LargeDevice tab={tabs}></LargeDevice>
+
+      {/* Medium Device Sidebar*/}
+      <MediumDevice tab={tabs}></MediumDevice>
+    </>
+  );
+};
+
+export default Sidebar;
 
