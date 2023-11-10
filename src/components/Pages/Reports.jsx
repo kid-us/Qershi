43980b98c9c7de608ecd218c0943178cca4870f0
Kidus <98009847+kid-us@users.noreply.github.com>
2023-11-10 12:25:36 -0800

    Update Reports.jsx

--- src/components/Pages/Reports.jsx
+++ src/components/Pages/Reports.jsx
@@ -1,1 +1,20 @@
+import React from "react";
+import Sidebar from "../Sidebar";
+import Searchbar from "../Contents/Searchbar";
+const Reports = () => {
+  return (
+    <>
+      <Sidebar></Sidebar>
 
+      <div className="main-contents">
+        <div className="row">
+          <div className="col-12">
+            <Searchbar></Searchbar>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Reports;
