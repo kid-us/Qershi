4033649ad6a5e0392df33e9ab4487e9a2d2c3cd2
Kidus <98009847+kid-us@users.noreply.github.com>
2023-11-12 12:55:07 -0800

    Update Button.jsx

--- src/components/Contents/Button.jsx
+++ src/components/Contents/Button.jsx
@@ -1,1 +1,12 @@
 
+import React from "react";
+
+const Button = ({ name, style }) => {
+  return (
+    <>
+      <button className={`${style} me-5`}>{name}</button>
+    </>
+  );
+};
+
+export default Button;
