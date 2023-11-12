85fd419e460bd79779bdd4afcf41bddcfdb3c86c
Kidus <98009847+kid-us@users.noreply.github.com>
2023-11-12 12:54:01 -0800

    Update tabs.js

--- src/components/constant/tabs.js
+++ src/components/constant/tabs.js
@@ -1,1 +1,8 @@
-c
+export const tabs = [
+  { title: "dashboard", index: 0, icon: "microsoft" },
+  { title: "transactions", index: 1, icon: "arrow-left-right" },
+  { title: "groups", index: 2, icon: "people-fill" },
+  // { title: "subscription", index: 3, icon: "bell-fill" },
+  { title: "reports", index: 4, icon: "graph-up-arrow" },
+  { title: "setting", index: 5, icon: "gear-fill" },
+];
