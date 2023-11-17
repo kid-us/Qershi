319840ca07059880f3097f1e09ac1edddadba092
Kidus <98009847+kid-us@users.noreply.github.com>
2023-11-17 12:31:57 -0800

    Update Auth.jsx

--- src/components/utils/Auth.jsx
+++ src/components/utils/Auth.jsx
@@ -1,1 +1,36 @@
+import { useState, createContext, useContext, useEffect } from "react";
+
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [phone, setPhone] = useState();
+
+  useEffect(() => {
+    if (sessionStorage.getItem("token")) {
+      setPhone(sessionStorage.getItem("token"));
+    } else {
+      setPhone(null);
+    }
+  }, [phone]);
+
+  const login = (phone) => {
+    sessionStorage.setItem("token", phone);
+    setPhone(phone);
+  };
+
+  const logout = () => {
+    sessionStorage.removeItem("token");
+    setPhone(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ phone, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
 
