4596646b9e1e387290edc6d4a3740a5f2d62cf3c
Kidus <98009847+kid-us@users.noreply.github.com>
2023-11-17 12:32:33 -0800

    Update RequireAuth.jsx

--- src/components/utils/RequireAuth.jsx
+++ src/components/utils/RequireAuth.jsx
@@ -1,1 +1,17 @@
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "./Auth";
+const RequireAuth = ({ children }) => {
+  const location = useLocation();
+  const auth = useAuth();
 
+  if (!sessionStorage.getItem("token")) {
+    if (!auth.phone) {
+      return <Navigate to="/" state={{ path: location.pathname }} />;
+    }
+  }
+
+  return children;
+};
+
+export default RequireAuth;
