fc553b247c468f680223810dd762656cbb12c0db
Kidus <98009847+kid-us@users.noreply.github.com>
2023-11-26 11:38:00 -0800

    Update SearchInput.jsx

--- src/components/Contents/SearchInput.jsx
+++ src/components/Contents/SearchInput.jsx
@@ -1,1 +1,19 @@
+import React from "react";
+
+const SearchInput = () => {
+  return (
+    <>
+      <div className="input-group bg rounded py-1 ps-4">
+        <input
+          className="form-control text-white"
+          name="search"
+          placeholder="Search for Users, Groups..."
+        />
+        <button className="input-group-text px-5 bi-search"></button>
+      </div>
+    </>
+  );
+};
+
+export default SearchInput;
 
